544595f93d56f6de61516813769489ac5083652c
Victor <victor.quertelet@gmail.com>
2025-04-03 14:44:27 +0200

    Delete unused useState

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import BoardsList from '@/components/boards/BoardsList';
 import { useKanbanStore } from '@/store/useKanbanStore';
 
